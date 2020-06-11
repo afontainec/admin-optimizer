4365eba7bf80cbb001a9ffa9ceb2a15f17e35b5e
antonio <afontaine@uc.cl>
2020-06-11 12:20:15 -0400

    filter todays files

---
+++ index.js
@@ -0,0 +1,21 @@
+const sii = require('./sii');
+// const spreadsheet = require('./spreadsheet');
+// const bank = require('./bank');
+
+const f = async () => {
+  const facturas = await sii.readFacturas();
+  // await spreadsheet.addFacturas(facturas);
+  // await spreadsheet.mapFacturas();
+  // const cartola = await bank.readCartola();
+  // await spreadsheet.addCartola(cartola);
+  // await spreadsheet.mapCartola();
+};
+
+
+f().then(() => {
+  console.log('TERMINADO CON EXITO');
+}).catch((err) => {
+  console.log(err);
+}).finally(() => {
+  process.exit();
+});
---
+++ sii.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const fs = require('fs');
+
+const ONE_DAY = 24 * 60 * 60 * 1000;
+const THRESHOLD = new Date().getTime() - ONE_DAY;
+const DOWNLOADS_PATH = path.join(__dirname, '..', '..', '..', '..', '..', '..', 'Downloads');
+
+const readFacturas = async () => {
+  const files = await todayFiles(DOWNLOADS_PATH);
+  console.log(files);
+};
+
+
+const todayFiles = (dirname) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirname, (err, filenames) => {
+      if (err) return reject(err);
+      const files = [];
+      filenames.forEach((filename) => {
+        if (!isSiiFile(filename)) return;
+        const stats = fs.statSync(dirname + path.sep + filename, 'utf-8');
+        if (shouldGoFile(stats)) files.push(filename);
+      });
+      return resolve(files);
+    });
+  });
+};
+
+const isSiiFile = (filename) => {
+  return filename.startsWith('DTE_') && filename.endsWith('.csv');
+};
+
+const shouldGoFile = (stats) => {
+  return stats.birthtime.getTime() > THRESHOLD;
+};
+
+
+module.exports = {
+  readFacturas,
+};
