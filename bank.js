a132c5e7e3582714fbbf6f2b0bb88a7bb3ba4723
antonio <afontaine@uc.cl>
2020-06-15 13:18:48 -0400

    getNew Movements

--- bank.js
+++ bank.js
@@ -6,6 +6,7 @@ const spreadsheet = require('./spreadsheet');
 
 const CARTOLA_PATH = path.join(__dirname, 'bank.txt');
 const HEADERS = ['Fecha', 'Oficina', 'Descripción', 'Nº Documento', 'Cargo', 'Abono', 'Saldo'];
+const AMOUNT_HEADERS = ['Cargo', 'Abono', 'Saldo'];
 const BANK_CARTOLA_RANGE = 'Cartola Real!A4:G';
 
 const readNewMovements = () => {
@@ -24,33 +25,77 @@ const addLine = (movements, line) => {
   const entry = {};
   for (let i = 0; i < HEADERS.length; i++) {
     const header = HEADERS[i];
-    const element = sii.isDate(elements[i]) ? sii.parseToDate(elements[i]) : elements[i];
+    let element = sii.isDate(elements[i]) ? sii.parseToDate(elements[i]) : elements[i];
+    if (isAnAmount(header)) element = parseAmountToInteger(element);
     entry[header] = element;
   }
   movements.push(entry);
 };
 
 
+const isAnAmount = (header) => {
+  return AMOUNT_HEADERS.indexOf(header) > -1;
+};
+
+const parseAmountToInteger = (element) => {
+  if (element === '') return 0;
+  element = element.replace(/\./g, '');
+  element = element.replace(/,/g, '');
+  return Number.parseInt(element, 10);
+};
+
 const mapMovements = async (movements) => {
   const sheets = await spreadsheet.connect();
   const bankCartola = await getBankCartola(sheets);
-  console.log(bankCartola);
-  // const toMap = addEntries(cartola, current);
+  const newMovements = getNewMovements(movements, bankCartola);
+  console.log(newMovements);
+  // const mapped = await mapNewMovements();
+  // await updateMapped(mapped);
+  // await insertMovements();
+  // printMovementSummary();
   // mapEntries(toMap);
 };
 
-const getBankCartola = (sheets) => {
-  return spreadsheet.read(sheets, BANK_CARTOLA_RANGE, HEADERS);
-
+const getBankCartola = async (sheets) => {
+  const result = await spreadsheet.read(sheets, BANK_CARTOLA_RANGE, HEADERS);
+  for (let i = 0; i < result.length; i++) {
+    const element = result[i];
+    for (let j = 0; j < AMOUNT_HEADERS.length; j++) {
+      const header = AMOUNT_HEADERS[j];
+      element[header] = parseAmountToInteger(element[header]);
+    }
+  }
+  return result;
 };
 
-// // const addEntries = () => {
-// //   for(entry)
-// //   const option = getBestOption(...);
-// //   if(!option) skip();
-// //   if(option.date === lastDate) askIfIsSame();
-// //   add(option);
-// // }
+const getNewMovements = (movements, bankCartola) => {
+  const hashTable = toHashTable(bankCartola);
+  const newMovements = [];
+  for (let i = 0; i < movements.length; i++) {
+    const movement = movements[i];
+    const key = hash(Object.values(movement));
+    const bestFit = hashTable[key];
+    if (!bestFit) {
+      newMovements.push(movement);
+      bankCartola.push(movement);
+    }
+  }
+  return newMovements;
+};
+
+const toHashTable = (bankCartola) => {
+  const table = {};
+  for (let i = 0; i < bankCartola.length; i++) {
+    const element = bankCartola[i];
+    const key = hash(Object.values(element));
+    table[key] = element;
+  }
+  return table;
+};
+
+const hash = (value) => {
+  return value.join('_');
+};
 
 
 module.exports = {
