93b95c0ae299830cdb557e900f6a5191c85a75f2
antonio <afontaine@uc.cl>
2020-06-15 12:16:54 -0400

    refactor

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const sii = require('./sii');
 const spreadsheet = require('./spreadsheet');
+const factura = require('./facturas');
 const bank = require('./bank');
 
 const f = async () => {
-  // console.log('----------------LEYENDO SSI');
-  // const facturas = await sii.readFacturas();
-  // console.log('facturas obtenidas');
-  // console.log('---------------- AGREGANDO FACTURAS');
-  // await spreadsheet.addFacturas(facturas);
+  console.log('----------------LEYENDO SSI');
+  const facturas = await sii.readFacturas();
+  console.log('facturas obtenidas');
+  console.log('---------------- AGREGANDO FACTURAS');
+  await factura.add(facturas);
   console.log('---------------- LEYENDO CARTOLA');
   const cartola = await bank.readCartola();
   await spreadsheet.addCartola(cartola);
